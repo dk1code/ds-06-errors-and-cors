6c35b7453435598ee745fc67c653516c4da36431
dk1code <dk1code@web.de>
2020-10-16 18:06:10 +0200

    done till generic error handler

--- server.js
+++ server.js
@@ -1,16 +1,36 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const morgan = require("morgan");
 
-app.get('/getme', (req, res) => {
-  res.send(fruits)
+app.use(morgan("dev"));
+app.use(express.json());
+
+app.get("/getme", (req, res) => {
+  res.send(fruits);
 });
 
-app.post('/postme', (req, res) => {
-  res.send({})
-})
+app.post("/postme", (req, res) => {
+  res.send({});
+});
+
+app.get("/fuckup/saw-it-coming", (req, res, next) => {
+  try {
+    test;
+  } catch (err) {
+    next(err)
+  }
+});
+
+app.get("/fuckup/surprise", (req, res) => {
+  throw new Error("Error thrown")
+});
+
+app.use((req, res) => {
+  res.status(404).json({ error: `Route ${req.url} does not exist` });
+});
 
 app.listen(5000, () => {
-  console.log('Example app listening on port 5000!');
+  console.log("Example app listening on port 5000!");
 });
 
 //Run app, then load http://localhost:5000 in a browser to see the output.
